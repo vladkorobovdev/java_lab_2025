c6ae69cbcc2378cbbd009047f442a14392685522
Uladzislau <umcs@gmail.com>
2025-02-28 09:40:21 +0100

    zadanie 2

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         Point p = new Point();
         p.x = 5.0;
         p.y = 7.5;
-        System.out.println(p);
+
+        System.out.println(p.toSvg());
     }
 }
--- svg/src/Point.java
+++ svg/src/Point.java
@@ -1,11 +1,18 @@
+import java.util.Locale;
+
 public class Point {
     public double x;
     public double y;
+
     @Override
     public String toString() {
         return "Point{" +
                 "x=" + x +
                 ", y=" + y +
                 '}';
+    }
+
+    public String toSvg() {
+        return String.format(Locale.ENGLISH, "<circle r=\"45\" cx=\"%.1f\" cy=\"%.1f\" fill=\"red\" />", this.x, this.y);
     }
 }
